1dbc95a2be5bcc346286ea5f79f78d748331ce54
zhanhuiping <2413373618@qq.com>
2024-10-02 19:42:15 +0800

    第一次提交

---
+++ src/main/java/com/zhp/taotaole/TaotaoleApplication.java
@@ -0,0 +1,13 @@
+package com.zhp.taotaole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaotaoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaotaoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhp/taotaole/TaotaoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhp.taotaole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaotaoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
